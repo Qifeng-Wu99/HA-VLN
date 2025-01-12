4056acf051fc102bf45ef9b25e05b8f04821f535
lpercc <lperlpm@gmail.com>
2025-01-12 20:59:45 +0800

    update

---
+++ Simulator/environments.py
@@ -0,0 +1,257 @@
+from typing import Any, Dict, Optional, Tuple, Union
+
+import habitat
+import numpy as np
+from habitat import Config, Dataset
+from habitat.core.simulator import Observations
+from habitat.tasks.utils import cartesian_to_polar
+from habitat.utils.geometry_utils import quaternion_rotate_vector
+from habitat_baselines.common.baseline_registry import baseline_registry
+
+from habitat_extensions.discrete_planner import DiscretePathPlanner
+from habitat_extensions.utils import generate_video, navigator_video_frame
+
+import json
+import os
+import threading
+import queue
+import time
+import magnum as mn
+import random
+from habitat.core.logging import logger
+import habitat_sim
+
+
+class HAVLNCE():
+    def __init__(self, config: Config, sim):
+        
+        self._config = config
+        self._sim = sim
+        # Initialize simulator resources
+        self._initialize_simulator_resources()
+        self.__init_manager__()
+
+        # Initialize total signals sent and previous human object IDs
+        self.total_signals_sent = 0  # Total number of signals sent
+        self.previous_human_object_ids = []  # Store object IDs to remove later
+
+        self.recomputed_navmesh = False
+
+        # Initialize a lock for thread safety
+        self.human_model_lock = threading.Lock()
+
+        # Initialize the signal queue for thread communication, maxsize=120
+        self.signal_queue = queue.Queue(maxsize=120)
+
+        # Start the child thread to send signals
+        self._signal_thread = threading.Thread(target=self._signal_sender)
+        self._signal_thread.daemon = True
+        self._signal_thread.start()
+
+    def reset(self) -> None:
+
+        # Clear signal queue and reset counters
+        self._reset_signal_queue_and_counters()
+
+        self.signal_queue.put('REFRESH_HUMAN_MODEL', timeout=1)
+        self.total_signals_sent += 1  # Increment total signals sent
+
+        # self._handle_signals()
+        self.add_new_human_model(frame_id=0)
+
+    
+    
+    def _reset_signal_queue_and_counters(self):
+        """Reset the signal queue and related counters before starting a new episode."""
+        # Clear the signal queue
+        with self.signal_queue.mutex:
+            self.signal_queue.queue.clear()
+        # logger.info("Signal queue cleared.")
+
+        # Reset total signals sent counter
+        self.total_signals_sent = 0
+        # logger.info("Total signals sent counter reset to 0.")
+
+        # Reset frame counter if used
+        self.frame_counter = 0
+        # logger.info("Frame counter reset to 0.")
+
+        # Remove any existing human models
+        self.remove_previous_human_model()
+        # logger.info("Previous human models removed.")
+        self.frame_id = 0
+
+        self.recomputed_navmesh = False
+
+
+    def _initialize_simulator_resources(self):
+        """Initialize resources required by the simulator."""
+        # Load human motion data and other resources
+        self.human_motion_json_path = self._config.SIMULATOR.HUMAN_INFO_PATH
+        self.data_path = self._config.SIMULATOR.HUMAN_GLB_PATH
+        self.nav_mesh_path = self._config.SIMULATOR.RECOMPUTE_NAVMESH_PATH
+
+        with open(self.human_motion_json_path, 'r') as f:
+            self.human_motion_data = json.load(f)
+
+        self.obj_templates_mgr = self._sim.get_object_template_manager()
+        logger.info("Simulator resources initialized.")
+
+    def _signal_sender(self):
+        """Child thread function to send signals to the main thread every 0.5 seconds."""
+        logger.info("Signal sender thread started.")
+        while True:
+            try:
+                # Send a signal to refresh the human model if queue is not full
+                self.signal_queue.put('REFRESH_HUMAN_MODEL', timeout=1)
+                self.total_signals_sent += 1  # Increment total signals sent
+                # logger.info(f"Signal sent to main thread to refresh human model. Total signals sent: {self.total_signals_sent}")
+            except queue.Full:
+                # logger.warning("Signal queue is full. Cannot send more signals.")
+                pass
+            # Wait for 0.5 seconds before sending the next signal
+            time.sleep(0.1)
+
+    def _handle_signals(self):
+        """Main thread method to handle signals from the child thread."""
+        try:
+            signals_processed = 0
+            while not self.signal_queue.empty():
+                signal = self.signal_queue.get_nowait()
+                signals_processed += 1
+                # No need to process the signal content, we just count them
+            # if signals_processed > 0:
+            #     # Calculate frame ID based on total signals sent
+            #     frame_id = (self.total_signals_sent - 1) % 120
+            #     logger.info(f"Processing {signals_processed} signals. Using frame {frame_id}")
+            #     self.refresh_human_model(frame_id)
+            if signals_processed > 0:
+                # Calculate frame ID based on total signals sent
+                frame_id = (self.total_signals_sent - 1) % 120
+                
+                if self.total_signals_sent <= 120:
+                    # logger.info(f"Processing {signals_processed} signals. Using frame {frame_id}")
+                    # if not self.recomputed_navmesh:
+                    #     self.refresh_human_model(frame_id)
+                    #     self.frame_id = frame_id
+                    self.refresh_human_model(frame_id)
+                    self.frame_id = frame_id
+                    scan = self._sim._current_scene.split('/')[-2]
+                    # self._sim.pathfinder.load_nav_mesh(os.path.join(self.nav_mesh_path, scan, scan + f'_{self.frame_id:03d}.navmesh'))
+                    if not os.path.exists(os.path.join(self.nav_mesh_path, scan)):
+                        os.makedirs(os.path.join(self.nav_mesh_path, scan))
+                    if not os.path.exists(os.path.join(self.nav_mesh_path, scan, scan + f'_{self.frame_id:03d}.navmesh')):
+                        navmesh_settings = habitat_sim.nav.NavMeshSettings()
+                        navmesh_settings.set_defaults()
+                        navmesh_settings.agent_radius = 0.1
+                        navmesh_settings.agent_height = 1.5
+                        state = self._sim.recompute_navmesh(self._sim.pathfinder, navmesh_settings, include_static_objects=True)
+                        assert state 
+                        state = self._sim.pathfinder.save_nav_mesh(os.path.join(self.nav_mesh_path, scan, scan + f'_{self.frame_id:03d}.navmesh'))
+                        assert state 
+                    else:
+                        self._sim.pathfinder.load_nav_mesh(os.path.join(self.nav_mesh_path, scan, scan + f'_{self.frame_id:03d}.navmesh'))
+                    
+        except Exception as e:
+            logger.error(f"An error occurred while handling signals: {e}")
+
+    def refresh_human_model(self, frame_id):
+        """Method to refresh the human model (remove previous and add new one)."""
+        # Remove previous human model
+        self.remove_previous_human_model()
+        # Add new human model with the specified frame_id
+        self.add_new_human_model(frame_id)
+
+    def remove_previous_human_model(self):
+        """Remove the previous human model from the simulator."""
+        # logger.info("Removing previous human model.")
+        with self.human_model_lock:
+            if self.previous_human_object_ids:
+                existing_ids = set(self._sim.get_existing_object_ids())
+                for obj_id in self.previous_human_object_ids:
+                    if obj_id in existing_ids:
+                        self._sim.remove_object(obj_id)
+                        # logger.info(f"Removed object with ID {obj_id}")
+                    else:
+                        logger.warning(f"Object ID {obj_id} does not exist in simulator.")
+                        # raise
+                self.previous_human_object_ids = []
+
+    def add_new_human_model(self, frame_id):
+        """Add a new human model to the simulator."""
+        # logger.info(f"Adding new human model at frame {frame_id}.")
+        scan = self._sim._current_scene.split('/')[-2]
+        human_motions = self.human_motion_data.get(scan, {})
+        # frame_id is passed as an argument
+        human_positions = {}
+
+        with self.human_model_lock:
+            self.previous_human_object_ids = []  # Reset the list for current objects
+
+            for viewpoint, viewpoint_data in human_motions.items():
+                category = viewpoint_data["category"]
+                glb_index = viewpoint_data["index"]
+                # Get translation and rotation for the specified frame
+                translation = viewpoint_data["translation"][frame_id]
+                rotation_euler = viewpoint_data["rotation"][frame_id]
+
+                category = category.replace(" ", "_").replace("/", "_")
+
+                # template_id = self.obj_templates_mgr.load_configs('/datadrive/qih/habitat-sim/data/test_assets/objects/nested_box.object_config.json')[0]
+
+                template_id = self.obj_templates_mgr.get_template_handle_by_ID(self.or_num +
+                                                (self.category2idx[f'{category}_{glb_index}'])*120+frame_id)
+
+                if translation is not None:
+                    translations = mn.Vector3(translation)
+                    rotation_quat = (
+                        mn.Quaternion.rotation(mn.Deg(rotation_euler[0]), mn.Vector3(1.0, 0.0, 0.0)) *
+                        mn.Quaternion.rotation(mn.Deg(rotation_euler[1]), mn.Vector3(0.0, 1.0, 0.0)) *
+                        mn.Quaternion.rotation(mn.Deg(rotation_euler[2]), mn.Vector3(0.0, 0.0, 1.0))
+                    )
+                    # object_id = self._sim.add_object(template_id)
+                    object_id = self._sim.add_object_by_handle(template_id)
+                    # logger.info(f"Added object with ID {object_id}")
+
+                    self._sim.set_translation(translations, object_id)
+                    self._sim.set_rotation(rotation_quat, object_id)
+                    rotation_quat = self._sim.get_rotation(object_id)
+                    human_positions[viewpoint] = (np.array(translation), rotation_euler)
+                    # print(rotation_euler, rotation_quat, rotation_quat.to_matrix())
+
+                    self._sim.set_object_motion_type(habitat_sim.physics.MotionType.STATIC, object_id)
+
+                    self._sim.step_physics(1.0 / 5.0)
+                    self.previous_human_object_ids.append(object_id)
+            
+            self._sim._human_posisions = human_positions
+            # num_objects = len(self.previous_human_object_ids)
+            # logger.info(f"Added {num_objects} objects")
+
+            # navmesh_settings = habitat_sim.nav.NavMeshSettings()
+            # navmesh_settings.set_defaults()
+            # navmesh_settings.agent_radius = 0.1
+            # navmesh_settings.agent_height = 1.5
+            # self._sim.recompute_navmesh(self._sim.pathfinder, navmesh_settings, include_static_objects=True)
+            # self.recomputed_navmesh = True
+    
+    def __init_manager__(self):
+        
+        
+        # data_path = "/datadrive/vln_share/habitat-sim/data/"
+
+        self.obj_templates_mgr = self._sim.get_object_template_manager()
+        
+        idx2category = {}
+        self.category2idx = {}
+        pre_num = self.obj_templates_mgr.get_num_templates()
+        self.or_num = pre_num
+
+        glb_lists = sorted(os.listdir(self.data_path))
+        for i, category in enumerate(glb_lists):
+            idx2category[i] = category
+            self.category2idx[category] = i
+            self.obj_templates_mgr.load_configs(str(f"{self.data_path}/{category}/"))
+            add_num = self.obj_templates_mgr.get_num_templates() - pre_num
+            assert add_num == 120
+            pre_num = self.obj_templates_mgr.get_num_templates()
---
+++ Simulator/measures.py
@@ -0,0 +1,124 @@
+
+from typing import Any, List, Union
+
+import numpy as np
+
+from habitat.config import Config
+from habitat.core.embodied_task import Action, EmbodiedTask, Measure
+from habitat.core.logging import logger
+from habitat.core.registry import registry
+from habitat.core.simulator import Simulator
+from habitat.core.utils import try_cv2_import
+from numpy import ndarray
+
+cv2 = try_cv2_import()
+
+
+def euclidean_distance(
+    pos_a: Union[List[float], ndarray], pos_b: Union[List[float], ndarray]
+) -> float:
+    return np.linalg.norm(np.array(pos_b) - np.array(pos_a), ord=2)
+
+
+@registry.register_measure
+class DistanceToHuman(Measure):
+    """The measure calculates a distance towards the human."""
+
+    cls_uuid: str = "distance_to_human"
+
+    def __init__(
+        self, sim: Simulator, config: Config, *args: Any, **kwargs: Any
+    ):
+        self._previous_position: Optional[Tuple[float, float, float]] = None
+        self._sim = sim
+        self._config = config
+        self._episode_view_points: Optional[
+            List[Tuple[float, float, float]]
+        ] = None
+
+        super().__init__(**kwargs)
+
+    def _get_uuid(self, *args: Any, **kwargs: Any) -> str:
+        return self.cls_uuid
+
+    def reset_metric(self, episode, *args: Any, **kwargs: Any):
+        self._previous_position = None
+        self._metric = {}
+        self.update_metric(episode=episode, *args, **kwargs)  # type: ignore
+
+    def update_metric(
+        self, episode, *args: Any, **kwargs: Any
+    ):
+        state = self._sim.get_agent_state()
+        current_position = state.position
+        current_rotation = state.rotation
+        human_positions = self._sim._human_posisions
+        for name, (position, rotation) in human_positions.items():
+            distance_to_human = euclidean_distance(
+                    current_position,
+                    position,
+                )
+            import quaternion
+            import math
+
+            agent = self._sim.get_agent(0)
+            action = agent.agent_config.action_space[1]
+
+            if agent.controls.is_body_action(action.name):
+                did_collide = agent.controls.action(
+                    agent.scene_node, action.name, action.actuation, apply_filter=True
+                )
+            else:
+                for v in agent._sensors.values():
+                    habitat_sim.errors.assert_obj_valid(v)
+                    agent.controls.action(
+                        v.object, action.name, action.actuation, apply_filter=False
+                    )
+            
+            move_position = self._sim.get_agent_state().position
+            
+            move = move_position - current_position
+            human_to_agent = position - current_position
+            move = move / (np.sqrt((move**2).sum()) + 1e-8)
+            human_to_agent = human_to_agent / (np.sqrt((human_to_agent**2).sum()) + 1e-8)
+            cos = (move*human_to_agent).sum()
+            assert cos <=1 and cos >= -1
+
+            theta = math.degrees(math.acos(cos))
+
+            agent.set_state(state)
+            # if did_collide:
+            #     print(move_position, current_position)
+            #     print(euclidean_distance(move_position, current_position))
+            #     raise
+
+            self._metric[name] = (distance_to_human, theta)
+
+        self._previous_position = current_position
+
+
+
+@registry.register_measure
+class CollisionsDetail(Measure):
+    def __init__(self, sim, config, *args: Any, **kwargs: Any):
+        self._sim = sim
+        self._config = config
+        self._metric = None
+        super().__init__()
+
+    def _get_uuid(self, *args: Any, **kwargs: Any) -> str:
+        return "collisions_detail"
+
+    def reset_metric(self, episode, *args: Any, **kwargs: Any):
+        self._metric = None
+
+    def update_metric(self, episode, action, *args: Any, **kwargs: Any):
+        if self._metric is None:
+            self._metric = {"count": 0, "is_collision": False, 'steps': []}
+        self._metric["is_collision"] = False
+        if self._sim.previous_step_collided:
+            self._metric["count"] += 1
+            self._metric["is_collision"] = True
+            self._metric["steps"].append(True)
+        else:
+            self._metric["steps"].append(False)
